703a705e29cab8b2b1263068e251810331ac3193
theut321 <jairus_villafranca@dlsu.edu.ph>
2026-04-08 16:36:36 +0800

    removed delete sa vists and all

--- php/delete_attraction.php
+++ php/delete_attraction.php
@@ -6,7 +6,6 @@ require 'db.php';
 
 $attraction_id = $_GET['attraction_id'];
 
-$delete_visits = $conn->query("DELETE FROM visits WHERE attraction_id= '$attraction_id'");
 $delete = $conn->query("DELETE FROM attraction WHERE attraction_id = '$attraction_id'");
 
 header("Location: read_attraction.php");
--- php/delete_user.php
+++ php/delete_user.php
@@ -5,7 +5,6 @@ require 'db.php';
 
 
 $user_id= $_GET['user_id'];
-$delete_visits = $conn->query("DELETE FROM visits WHERE user_id= '$user_id'");
 $delete = $conn ->query("DELETE FROM user WHERE user_id = '$user_id'");
 header("Location: read_user.php");
 exit();
