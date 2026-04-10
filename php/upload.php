6531061d94752bb64695b0ca24825d592817d5e2
Faith Francisco <faith.francisco2006@gmail.com>
2026-04-10 21:14:26 +0800

    Implemented role and session checks and redirection to right pages

--- php/upload.php
+++ php/upload.php
@@ -2,7 +2,8 @@
 session_start();
 require 'db.php';
 
-if (!isset($_SESSION['user_id'])) {
+// strictly only allow logged in users with "user" role to upload images
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'user') {
     header("Location: login.php");
     exit();
 }
